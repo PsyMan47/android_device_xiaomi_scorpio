efc54443fbda91e26a04580a5537642ef88d84b7
PsyMan47 <xhonirexhepi@gmail.com>
2017-11-02 23:54:37 +0000

    Bring in AOSP

--- aosp_scorpio.mk
+++ aosp_scorpio.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/xiaomi/scorpio/full_scorpio.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
-PRODUCT_NAME := lineage_scorpio
+PRODUCT_NAME := aosp_scorpio
 PRODUCT_DEVICE := scorpio
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI Note 2
